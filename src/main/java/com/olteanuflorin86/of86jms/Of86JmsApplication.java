9fa17b9c407d668bbfd373ca83e8e1162fafd2dd
Florin Olteanu <olteanuflorin86@gmail.com>
2023-06-09 17:19:40 +0300

    Initial commit - JMS project setup

---
+++ src/main/java/com/olteanuflorin86/of86jms/Of86JmsApplication.java
@@ -0,0 +1,13 @@
+package com.olteanuflorin86.of86jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Of86JmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Of86JmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olteanuflorin86/of86jms/Of86JmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olteanuflorin86.of86jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Of86JmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
